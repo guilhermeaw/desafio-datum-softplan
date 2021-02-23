e231be5c1cf06c097bfbbc4c6b773927c5416730
guilhermeaw <guilherme.w@universo.univates.br>
2021-02-22 22:52:57 -0300

    create global style

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #f5f5f5;
+    color: #000;
+    -webkit-font-smoothing: antialiased;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
