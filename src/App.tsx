ba107556a8101a907087469648c8dd004d293619
guilhermeaw <guilherme.w@universo.univates.br>
2021-02-24 20:24:13 -0300

    create apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_APOLLO_CLIENT_URI,
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <ApolloProvider client={client}>
+        <Routes />
+      </ApolloProvider>
 
       <GlobalStyle />
     </BrowserRouter>
